1bb27fb8753dedec373ddf1e7e0173a74d6356aa
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2021-07-16 21:49:36 +0600

    Project Added on Solution,Entities and Migration Done

---
+++ AuctionSystem.Database/AuctionSystemContext.cs
@@ -0,0 +1,16 @@
+﻿using AuctionSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionSystem.Database
+{
+    public class AuctionSystemContext:DbContext
+    {
+        
+        public DbSet<Auction> Auctions { get; set; }
+    }
+}
---
+++ AuctionSystem.Database/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace AuctionSystem.Database.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AuctionSystem.Database.AuctionSystemContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(AuctionSystem.Database.AuctionSystemContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
